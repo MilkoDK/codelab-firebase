889e56fb98b9d37f3fd257a2ccb08f7c55c14730
MIlkoDK <bducvj@gmail.com>
2024-11-20 21:54:45 -0400

    feat: Multiplatform Google Auth Sign In

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart'; // new
 import 'package:flutter/material.dart';
 
 import 'home.dart';
@@ -16,6 +17,7 @@ class AuthGate extends StatelessWidget {
          return SignInScreen(
            providers: [
              EmailAuthProvider(),
+             GoogleProvider(clientId: "578987021233-t6826kmucssl6cr367a3mc3otevab7mn.apps.googleusercontent.com"),  // new
            ],
            headerBuilder: (context, constraints, shrinkOffset) {
              return Padding(
@@ -54,6 +56,7 @@ class AuthGate extends StatelessWidget {
            },
          );
        }
+
        return const HomeScreen();
      },
    );
